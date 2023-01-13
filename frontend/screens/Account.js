9942cb7d28ced6fa99f4a62db2bd1296a67793a8
JsnSwny <jsnswny@gmail.com>
2023-01-13 15:05:49 +0000

    Added temporary log out button to Account.js

--- frontend/screens/Account.js
+++ frontend/screens/Account.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import Title from "../components/Title";
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
+import { useAuth } from "../context/AuthContext";
 
 const Account = ({ navigation }) => {
+  const auth = useAuth();
   return (
     <React.Fragment>
       <Header title="coreer" />
       <View>
         <Title title="Account" />
       </View>
+      <Button title="Log out" onPress={() => auth.signOut()} />
     </React.Fragment>
   );
 };
