602a1209261e5540c9909cb8789b8eeabcd4cd77
JsnSwny <jsnswny@gmail.com>
2023-06-11 20:50:01 +0100

    Removed react native files and moved backend files to root

--- frontend/components/Container.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-
-const Container = ({ children }) => (
-  <View style={{ paddingHorizontal: 16 }}>{children}</View>
-);
-
-export default Container;
--- frontend/utils/capitalise.js
+++
@@ -1,5 +0,0 @@
-export default function capitalise(str) {
-  return str.replace(/\b\w/g, function (l) {
-    return l.toUpperCase();
-  });
-}
