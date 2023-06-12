ec8a4edea668181128a937c7e2f956958537530f
JsnSwny <jsnswny@gmail.com>
2023-06-12 14:45:17 +0100

    Set icon_name in Language model to optional

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Language(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    icon_name = models.CharField(max_length=255)
+    icon_name = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} -> {self.icon_name}"
